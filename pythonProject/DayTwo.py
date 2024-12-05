01006d681ac43f749431e8e49654f59f7108bbb9
NikitaAmerican <157661900+throwAwayingUp@users.noreply.github.com>
2024-12-05 14:41:48 -0500

    Add files via upload

--- pythonProject/DayTwo.py
+++ pythonProject/DayTwo.py
@@ -6,7 +6,34 @@ with open(file_path, "r") as f:
 
 fickleString = ""
 array = []
-
+safeCount = 0
 for i in content:
+    if i == " ":
+        array.append(int(fickleString))
+        fickleString = ""
     if i == "\n":
-        array += fickleString
+        safe = True
+
+        if array[len(array) - 1 ] > array[0]:
+            breed = "increasing"
+        if array[len(array) - 1] < array[0]:
+            breed = "decreasing"
+
+        for b in range(0, len(array)):
+            if b > 0:
+                if array[b] - array[b - 1] > 2 or array[b] - array[b - 1] < -2:
+                    safe = False
+                if array[b] > array[b - 1] and breed == "decreasing":
+                    safe = False
+                if array[b] < array[b - 1] and breed == "increasing":
+                    safe = False
+
+        if safe:
+            safeCount += 1
+        fickleString = ""
+        array = []
+
+    else:
+        fickleString += i
+
+print(safeCount)
