6cd3fba64cdd255500b788244089ba3cebb294f0
NikitaAmerican <157661900+throwAwayingUp@users.noreply.github.com>
2024-12-06 14:49:18 -0500

    Add files via upload

--- pythonProject/DayTwo.py
+++ pythonProject/DayTwo.py
@@ -12,24 +12,32 @@ for i in content:
         array.append(int(fickleString))
         fickleString = ""
     if i == "\n":
+
+        array.append(int(fickleString))
         safe = True
 
         if array[len(array) - 1 ] > array[0]:
             breed = "increasing"
         if array[len(array) - 1] < array[0]:
             breed = "decreasing"
-
-        for b in range(0, len(array)):
+        print(breed)
+        for b in range(len(array)):
             if b > 0:
-                if array[b] - array[b - 1] > 2 or array[b] - array[b - 1] < -2:
+                if array[b] - array[b - 1] > 3 or array[b] - array[b - 1] < -3:
                     safe = False
                 if array[b] > array[b - 1] and breed == "decreasing":
                     safe = False
                 if array[b] < array[b - 1] and breed == "increasing":
                     safe = False
+                if array[b] == array[b - 1]:
+                    safe = False
 
         if safe:
             safeCount += 1
+
+
+        #print(fickleString)
+        #print(array)
         fickleString = ""
         array = []
 
@@ -37,3 +45,7 @@ for i in content:
         fickleString += i
 
 print(safeCount)
+
+
+
+
