b556df809e54462895fdbd5886304f61242a9a72
NikitaAmerican <157661900+throwAwayingUp@users.noreply.github.com>
2024-12-09 12:20:21 -0500

    Add files via upload

--- pythonProject/DayTwo.py
+++ pythonProject/DayTwo.py
@@ -1,4 +1,5 @@
-file_path = "C:/Users/BT_4N2_18/PycharmProjects/pythonProject/input.txt"
+file_path = "C:/Users/BT_2N8_12/PycharmProjects/DayTwoAdvent/pythonProject/input.txt"
+
 with open(file_path, "r") as f:
     content = f.read()
 
@@ -16,6 +17,8 @@ for i in content:
         array.append(int(fickleString))
         safe = True
 
+        virgin = True
+
         if array[len(array) - 1 ] > array[0]:
             breed = "increasing"
         if array[len(array) - 1] < array[0]:
@@ -23,14 +26,35 @@ for i in content:
         print(breed)
         for b in range(len(array)):
             if b > 0:
-                if array[b] - array[b - 1] > 3 or array[b] - array[b - 1] < -3:
-                    safe = False
-                if array[b] > array[b - 1] and breed == "decreasing":
-                    safe = False
-                if array[b] < array[b - 1] and breed == "increasing":
-                    safe = False
                 if array[b] == array[b - 1]:
-                    safe = False
+                    if not virgin:
+                        safe = False
+                    if virgin:
+                         # array[b] = array[b - 1]
+                        virgin = False
+
+                if array[b] - array[b - 1] > 3 or array[b] - array[b - 1] < -3:
+                    if not virgin:
+                        safe = False
+                    if virgin:
+                        print(array)
+                        array[b] = array[b - 1]
+                        virgin = False
+                if array[b] > array[b - 1] and breed == "decreasing":
+                    if not virgin:
+                        safe = False
+                    if virgin:
+                        print(array)
+                        array[b] = array[b - 1]
+                        virgin = False
+
+                if array[b] < array[b - 1] and breed == "increasing":
+                    if not virgin:
+                        safe = False
+                    if virgin:
+                        print(array)
+                        array[b] = array[b - 1]
+                        virgin = False
 
         if safe:
             safeCount += 1
